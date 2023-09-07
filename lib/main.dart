ae9faf8ee55340a29a5c902927223a8b1beafad4
CsAdi <oradam86@gmail.com>
2023-09-07 12:29:55 +0200

    Finished the Chapter 5 - Provider - SMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -12,6 +13,52 @@ void main() {
   );
 }
 
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.yellow,
+        height: 100,
+        child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+        child: Text(minutes.value),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -21,6 +68,34 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: const Center(
           child: Text('My App'),
+        ),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(seconds: 5),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          ),
+        ],
+        child: const Column(
+          children: [
+            Row(
+              children: [
+                SecondsWidget(),
+                MinutesWidget(),
+              ],
+            )
+          ],
         ),
       ),
     );
